eea97472a99c820252f83785892f9bab8cbedc40
Yafa Putra <yafaputra@gmail.com>
2026-01-21 19:40:17 +0700

    Season-3

--- my-app/app/(landing)/order-status/[id]/page.tsx
+++ my-app/app/(landing)/order-status/[id]/page.tsx
@@ -19,7 +19,7 @@ const OrderStatus = async ({ params }: TPageProps) => {
 
   return (
     <main className="bg-gray-100 min-h-[80vh] pb-20">
-      <div className="max-w-5xl mx-auto pt-40 pb-20">
+      <div className="max-w-5xl mx-auto pt-40 pb-10">
         <h1 className="text-5xl font-bold text-center mb-11">Order Status</h1>
       </div>
       {transaction.status === "pending" && <OrderSubmitted />}
