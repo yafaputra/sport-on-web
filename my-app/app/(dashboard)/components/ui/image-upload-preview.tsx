4f80701d2a62be28a04054ec1d2b3508173b59df
Yafa Putra <yafaputra@gmail.com>
2026-02-04 10:59:04 +0700

    Season-4

--- my-app/app/(dashboard)/components/ui/image-upload-preview.tsx
+++ my-app/app/(dashboard)/components/ui/image-upload-preview.tsx
@@ -1,41 +1,69 @@
-import { useRef } from "react";
-import { FiUploadCloud } from "react-icons/fi";
 import Image from "next/image";
-import { on } from "events";
+import { useRef } from "react";
+import { FiEdit, FiUploadCloud } from "react-icons/fi";
+
 type TImageUploadPreviewProps = {
   label?: string;
   value?: string | null;
-  onChange?: (file: File) => void;
+  onChange: (file: File) => void;
   className?: string;
 };
 
-const ImageUploadPreview = ({ label, value, onChange, className }: TImageUploadPreviewProps) => {
- const fileInputRef = useRef<HTMLInputElement | null>(null);
+const ImageUploadPreview = ({
+  label,
+  value,
+  onChange,
+  className,
+}: TImageUploadPreviewProps) => {
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
 
- const handleImageChange = () => {
-  fileInputRef?.current?.click();
- };
- const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-  if (e.target.files && e.target.files[0]) {
-   const file = e.target.files[0];
-   onChange(file);
-  }
- };
+  const handleImageClick = () => {
+    fileInputRef?.current?.click();
+  };
 
- return (
-   <div className={className}>
-    <div onClick={handleImageChange} className="border-2 border-dashed border-prtimary bg-primary/5 rounded-b-lg h-50 flex flex-col justify-center items-center ">
-     {value ? (
-      <Image src={value} alt="Uploaded Image" width={190} height={190} className="object-cover h-full h-full"  />
-     ) : (
-     <>
-     <FiUploadCloud className ="text-primary" size={24}/>
-     <span className="text-sm font-medium">CClick to upload</span>
-     </>
-     )}
-    <input type="file" ref={fileInputRef} className="hidden" accept="image/*" onChange={handleFileChange}/>
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files[0]) {
+      const file = e.target.files[0];
+      onChange(file);
+    }
+  };
+
+  return (
+    <div className={className}>
+      <div
+        onClick={handleImageClick}
+        className="border-2 border-dashed border-primary bg-primary/5 rounded-lg h-50 flex flex-col justify-center items-center"
+      >
+        {value ? (
+          <div className="max-w-[190px] relative">
+            <Image
+              src={value}
+              alt="preview product"
+              className="w-full h-full object-cover"
+              width={190}
+              height={190}
+            />
+            <div className="opacity-0 hover:opacity-100 absolute top-0 left-0 flex z-50">
+              <FiEdit />
+              Change Image
+            </div>
+          </div>
+        ) : (
+          <>
+            <FiUploadCloud className="text-primary" size={24} />
+            <span className="text-sm font-medium">Click to Upload</span>
+          </>
+        )}
+        <input
+          type="file"
+          ref={fileInputRef}
+          className="hidden"
+          accept="image/*"
+          onChange={handleFileChange}
+        />
+      </div>
     </div>
-   </div>
-  )
+  );
 };
+
 export default ImageUploadPreview;
